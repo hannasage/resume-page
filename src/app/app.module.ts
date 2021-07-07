16062d9f5c863ccca14c102f1c6780b551d35e5e
kevinhaube <khaube@ametrics.org>
2021-07-07 15:20:49 -0400

    Hero area and text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
